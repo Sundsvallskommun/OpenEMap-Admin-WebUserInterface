f40589ba31fbb6c87d64d33d1c23d3d07b942273
Per Fahlén <per.fahlen@sweco.se>
2014-07-04 16:04:09 +0200

    Revert "added comments to java files"
    This reverts commit 6520f0685d15ff7abf378833a30d7647c21b4107.

--- Modules/src/org/oemap/services/modules/PublicLayersModule.java
+++ Modules/src/org/oemap/services/modules/PublicLayersModule.java
@@ -1,5 +1,5 @@
 /******************************************************************************
-Copyright Härnösands kommun(C) 2014 
+Copyright Härnösands kommun(C) 2014  <name of author>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as
