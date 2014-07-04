412a1d3b46ebaefbf67f93760a0427e66bf165bd
Per Fahlén <per.fahlen@sweco.se>
2014-07-04 15:35:15 +0200

    added comments to js files

--- UI/src/main/javascript/store/ToolStore.js
+++ UI/src/main/javascript/store/ToolStore.js
@@ -1,3 +1,24 @@
+/******************************************************************************
+Copyright Härnösands kommun(C) 2014 
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Affero General Public License
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/agpl-3.0.html
+ ******************************************************************************/
+
 Ext.define('AdmClient.store.ToolStore', {
 	extend : 'Ext.data.ArrayStore',
 
@@ -19,7 +40,6 @@ Ext.define('AdmClient.store.ToolStore', {
 	         [ 'DrawGeometry', 'Polygon', 'Action for draw polygon tool.', false ],
 			[ 'DeleteGeometry', 'Delete geometry', 'Tool for delete geometry.', false ],
 			[ 'DeleteAllFeatures', 'Delete all geometry', 'Tool for delete all geometries on map.', false ],
-			//[ 'DrawObject', 'Draw object', 'Tool for draw object i e, square, L-shape, hexagon-shape etc.', false ],
 			[ 'FullExtent', 'Full extent', 'Zoom to full extent.', false ],
 			[ 'Identify', 'Identify', 'Identify features.', false ],
 			[ 'MeasureArea', 'Measure area', 'Measure area in 2D.', false ],
--- UI/src/main/javascript/view/settings/SearchServersGrid.js
+++ UI/src/main/javascript/view/settings/SearchServersGrid.js
@@ -1,17 +1,30 @@
+/******************************************************************************
+Copyright Härnösands kommun(C) 2014 
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Affero General Public License
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/agpl-3.0.html
+ ******************************************************************************/
+
 Ext.define('AdmClient.view.settings.SearchServersGrid', {
 	extend : 'AdmClient.view.settings.SettingsGridBase',
 	alias : 'widget.SearchServersGrid',
 	requires : ['Ext.grid.*'],
 	
 	initComponent : function(){
-//		this.columns.add({
-//			
-//				text : 'ID',
-//				dataIndex : 'serverId',
-//				visible : false
-//			
-//		});
-//		this.columns.doLayout();
 		
 		this.callParent(arguments);
 	}
