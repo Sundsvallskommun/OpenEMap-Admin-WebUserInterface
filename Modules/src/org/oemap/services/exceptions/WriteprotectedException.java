6520f0685d15ff7abf378833a30d7647c21b4107
Per Fahlén <per.fahlen@sweco.se>
2014-07-04 15:38:56 +0200

    added comments to java files

--- Modules/src/org/oemap/services/exceptions/WriteprotectedException.java
+++ Modules/src/org/oemap/services/exceptions/WriteprotectedException.java
@@ -1,5 +1,5 @@
 /******************************************************************************
-Copyright Härnösands kommun(C) 2014  <name of author>
+Copyright Härnösands kommun(C) 2014 
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as
