6e069d62742b2b644993d05fd897621919756724
Anders Erlandsson <anders.erlandsson@sundsvall.se>
2015-04-23 09:47:12 +0200

    Fix for #39. Missed to check in some toolDetails files in previous check in

---
+++ UI/src/main/javascript/OpenEMapAdmin.js
@@ -0,0 +1,163 @@
+/*
+Copyright Härnösands kommun(C) 2014 
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    All rights reserved. This program and the accompanying materials
+    are made available under the terms of the GNU Affero General Public License
+    which accompanies this distribution, and is available at
+    http://www.gnu.org/licenses/agpl-3.0.html
+ */
+
+/**
+ * ###Factory for creating a Open eMap Admin application
+ *  
+ * Call the method initOpenEMapAdmin(configPath, options)
+ * ###[Integration example](https://github.com/Sundsvallskommun/OpenEMap-WebUserInterface/blob/master/README.md)
+ */
+var version = "-1.6";
+var openEMapScriptLocation;
+var appPath = '/openemapadmin'; 
+var defaultWMSServer = 'https://extmaptest.sundsvall.se/geoserver/wms?request=GetCapabilities&version=1.1.0';
+var wfsServer = 'https://extmaptest.sundsvall.se/geoserver/wfs';
+
+var loadCssFiles = function(files, callback) {
+	var fileCounter = 0;
+	var cssFilesIndex = document.styleSheets.length - 1;
+
+	for (var index in files) {
+		fileCounter++;
+		if(checkFileType(files[index])) {
+			var link = document.createElement("link");
+			link.href = files[index];
+			link.rel = "stylesheet";
+			link.type = "text/css";
+			link.onload = checkFinishedState;
+			if (this.readyState === 'loaded' || this.readyState === 'complete') {
+				link.onreadystatechange = checkFinishedState();
+			}
+			document.getElementsByTagName("head")[0].appendChild(link);
+			cssFilesIndex++;
+			if (!window.opera && navigator.userAgent.indexOf("MSIE") == -1) {
+				checkCSSLoadingState(cssFilesIndex);
+			}
+		}
+	}
+
+	function checkFileType(f) {
+		f = f.toLowerCase();
+	    var cssIndex = f.indexOf("css");
+	    if(cssIndex == -1) {
+	    	return false;
+	    }
+	    return true;
+	}
+	
+	function checkCSSLoadingState(index) {
+		try {
+			if (document.styleSheets[index].cssRules) {
+				checkFinishedState();
+			} else {
+				if (document.styleSheets[index].rules && document.styleSheets[index].rules.length) {
+					checkFinishedState();
+				} else {
+					setTimeout(function() { checkCSSLoadingState(index); }, 250);
+				}
+			}
+		} catch (e) {
+			setTimeout(function() { checkCSSLoadingState(index); }, 250);
+		}
+	}
+
+	function checkFinishedState() {
+		fileCounter--;
+		if (fileCounter === 0) {
+			callback();
+			fileCounter = -1;
+		}
+	}
+};
+
+var loadJsScripts = function(files) {
+	var head = document.getElementsByTagName("head")[0];
+	var waitUntilDependenciesIsLoaded = function(dependencies, script) {
+		if (dependencies()) {
+			head.appendChild(script);
+		} else {
+			setTimeout(function() { waitUntilDependenciesIsLoaded(dependencies, script); }, 5);
+		}
+	};
+	
+    for (var i = 0; i < files.length; i++) {
+		var script = {};
+		script = document.createElement('script');
+		script.type = 'text/javascript';
+	 	script.src = files[i].src;
+
+    	waitUntilDependenciesIsLoaded(files[i].dependencies, script);
+    }
+};
+
+var waitUntilOpenEMapIsLoaded = function(callback) {
+	if (typeof AdmClient === "undefined")  {
+		setTimeout(function() {waitUntilOpenEMapIsLoaded(callback);}, 5);
+	} else {
+		callback();
+	}
+};
+
+(function () {
+	var scriptNameRaw = "OpenEMapAdmin.js";
+	var openEMapScriptName = new RegExp("(^|(.*?\\/))(" + scriptNameRaw + ")(\\?|$)");
+	var scripts = document.getElementsByTagName("script");
+	for (var i = 0; i < scripts.length; i++) {
+		var src = scripts[i].getAttribute('src');
+		if (src) {
+			var match = src.match(openEMapScriptName);
+	        if (match) {
+	        	openEMapScriptLocation = match[1];
+	        	break;
+	        }
+	    }
+	}
+	var cssFiles = [
+		openEMapScriptLocation + "lib/ext/resources/ext-theme-neptune/ext-theme-neptune-all.css",
+		openEMapScriptLocation + "oeadmin.css"
+	];
+
+	scripts = [
+		{src: openEMapScriptLocation + "lib/ext/ext-all.js", dependencies: function() {return true;}},
+		{src: openEMapScriptLocation + "lib/ext/ext-theme-neptune.js", dependencies: function() {return (typeof Ext !== "undefined" && Ext.isReady);}},
+		{src: openEMapScriptLocation + "lib/ext/locale/ext-lang-sv_SE.js", dependencies:  function() {return (typeof Ext !== "undefined" && Ext.isReady);}},
+		{src: openEMapScriptLocation + "lib/OpenLayers/OpenLayers.js", dependencies: function() {return true;}},
+		{src: openEMapScriptLocation + "lib/geoext/geoext-all.js", dependencies: function() {return ((typeof Ext !== "undefined") && Ext.isReady &&  (typeof OpenLayers !== "undefined"));}},
+		{src: openEMapScriptLocation + "lib/OpenEMap/OpenEMap-min.js", dependencies: function() {return ((typeof Ext !== "undefined") && Ext.isReady &&  (typeof GeoExt !== "undefined") &&  (typeof OpenLayers !== "undefined"));}},
+		{src: openEMapScriptLocation + "OpenEMap-Admin-debug.js", dependencies: function() {return ((typeof Ext !== "undefined") && Ext.isReady &&  (typeof GeoExt !== "undefined") &&  (typeof OpenLayers !== "undefined") && (typeof OpenEMap !== "undefined") && (typeof OpenEMap.Client !== "undefined") );}}
+    ];
+	
+	// Ensure css files are loaded before js files
+	loadCssFiles(cssFiles, function() {
+		loadJsScripts(scripts);
+	});
+
+/*	waitUntilOpenEMapIsLoaded(run);
+	function run() {
+		AdmClient.app.admClient.setHeight(window.innerHeight - 70);
+		AdmClient.app.admClient.setWidth(window.innerWidth);
+		window.addEventListener("resize", function() {
+				AdmClient.app.admClient.setHeight(window.innerHeight - 70);
+				AdmClient.app.admClient.setWidth(window.innerWidth);
+		});
+	}
+*/
+}) ();
---
+++ UI/src/main/javascript/controller/toolDetails/ZoomSelector.js
@@ -0,0 +1,67 @@
+/*
+Copyright Härnösands kommun(C) 2014 
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    All rights reserved. This program and the accompanying materials
+    are made available under the terms of the GNU Affero General Public License
+    which accompanies this distribution, and is available at
+    http://www.gnu.org/licenses/agpl-3.0.html
+ */
+
+
+/**
+* A controller to handle tooldetails for delete measure tool.
+*/
+
+Ext.define('AdmClient.controller.toolDetails.ZoomSelector', {
+	extend : 'AdmClient.controller.MapConfiguration',
+	requires : [ 'AdmClient.view.mapconfiguration.tools.details.*', 'AdmClient.view.MapConfiguration'],
+	refs : [{
+		ref : 'toolsGrid',
+		selector : '#toolsGrid'
+	}],
+	init : function() {
+		this.control({
+			'#toolsGrid checkcolumn' : {
+				checkchange : this.toolSelected
+			}
+		});
+	},
+	
+	toolSelected : function(chkBox, rowIndex, checked, eOpts) {
+		var toolObject = this.getToolsGrid().getSelectionModel().store.data.items[rowIndex].data;
+		var tool = null;
+		if (/ZoomSelector/.test(toolObject.toolName)){
+			if (checked){
+				//find the right place in config object
+				var configItems = AdmClient.app.config.tools.filter(function(t){
+					return (t === 'ZoomSelector' || t.tool === 'ZoomSelector');
+				});
+				
+				if (configItems.length === 0){ // add tool to config object
+					tool = {type: 'ZoomSelector'};
+					AdmClient.app.config.tools.push(tool);
+				}
+			} else {
+				for (var i = 0; i < AdmClient.app.config.tools.length; i++){
+					tool = AdmClient.app.config.tools[i];
+					if (/ZoomSelector/.test(tool) || /ZoomSelector/.test(tool.type)){
+						AdmClient.app.config.tools.splice(i, 1);
+					}
+				}
+			}
+			this.getToolsGrid().store.commitChanges();
+		}
+	}
+});
